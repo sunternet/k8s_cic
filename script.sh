02a5a29689ba61179f95eb43afc2a28a6c1c378a
Todd Sun <sunternet@hotmail.com>
2020-11-24 17:31:32 +0800

    a

--- script.sh
+++ script.sh
@@ -104,7 +104,7 @@ kubectl create -f citrix-k8s-ingress-controller.yaml
 kubectl get pods
 kubectl describe pod cic-k8s-ingress-controller-xxx
 
-# Create a Ingress Controller
+# Create a Ingress
 kubectl apply -f hello-world-ingress.yaml
 kubectl get ingress
 
@@ -158,52 +158,126 @@ curl http://$NS_VIP/foobar --header 'Host: www.hello.com'
 # 2-Tier Deployment
 ##########################
 # Create a namespace for the environment
-kubectl create namespace 2t
+kubectl create namespace hello
+kubectl create namespace foobar
 
 # Create backend http server pods
-kubectl create deployment hello-world --image=gcr.io/google-samples/hello-app:1.0 -n 2t
-kubectl scale --replicas=3 deployment.apps/hello-world -n 2t
+kubectl create deployment hello-world --image=gcr.io/google-samples/hello-app:1.0 -n hello
+kubectl scale --replicas=2 deployment.apps/hello-world -n hello
+kubectl get pods -n hello -o wide
+kubectl get deployment -n hello -o wide
+
+kubectl create deployment foobar --image=k8s.gcr.io/echoserver:1.4 -n foobar
+kubectl scale --replicas=2 deployment.apps/foobar -n foobar
+kubectl get pods -n foobar -o wide
+kubectl get deployment -n foobar -o wide
 
 # expose as Headless Services
-kubectl apply -f headlessHTTPService.yaml -n 2t
+kubectl apply -f headlessHTTPService-hello.yaml -n hello
+kubectl get service -n hello
+
+kubectl apply -f headlessHTTPService-foobar.yaml -n foobar
+kubectl get service -n foobar
 
 # Let's first deploy the 2nd tier CPX
 # create rbac account for cpx
-kubectl get serviceaccount -n 2t
+kubectl apply -f rbac-cpx-hello.yaml -n hello
+kubectl get serviceaccount -n hello
+
+kubectl apply -f rbac-cpx-foobar.yaml -n foobar
+kubectl get serviceaccount -n foobar
+
+# Create a secret for storing the CPX login credential
+kubectl create secret generic nslogin --from-literal=username='nsroot' --from-literal=password='nsroot' -n hello
+kubectl create secret generic nslogin --from-literal=username='nsroot' --from-literal=password='nsroot' -n foobar
 
 # create cpx with cic as a sidecar and expose as a service
-kubectl apply -f cpx-cic-2t.yaml -n 2t
+# Port 80 will be exposed to get inbound traffic
+kubectl apply -f cpx-cic-2tier-hello.yaml -n hello
+kubectl get pods -o wide -n hello
+kubectl get deployment -n hello
+kubectl get service -n hello
+
+kubectl apply -f cpx-cic-2tier-foobar.yaml -n foobar
+kubectl get pods -n foobar
+kubectl get deployment -n foobar
+kubectl get service -n foobar
+
+# login to cpx and check the config
+kubectl exec -n hello -it cpx-cic-hello-[string] -- bash
+cli_script.sh "show ns run"
+cli_script.sh "show ns run" | grep k8s
+exit
 
 # create the ingress object
-kubectl apply -f ingress-2t.yaml -n 2t
+kubectl apply -f ingress-2tier-hello.yaml -n hello
+kubectl get ingress -n hello
+
+kubectl apply -f ingress-2tier-foobar.yaml -n foobar
+kubectl get ingress -n foobar
 
 # check the ingress
 # you will see backend http server listed in "Backends"
-kubectl describe ingress ingress-2t -n 2t
+kubectl describe ingress ingress-2tier-hello -n hello
+kubectl describe ingress ingress-2tier-foobar -n foobar
+
+# Check the CPX configuration
+kubectl exec -n hello -it cpx-cic-hello-[string] -- bash
+cli_script.sh "show ns run" | grep k8s
+exit
 
 # Access CPX lb service
-kubectl get pods -n 2t -o wide
+kubectl get pods -n hello -o wide
+CPX_POD_IP=`kubectl get pods -n hello -o wide | grep cpx-cic | awk '{ print $6 }'`
 curl http://$CPX_POD_IP --header 'Host: www.hello.com'
-kubectl get service -n 2t -o wide
+kubectl get service -n hello -o wide
+CPX_Cluster_IP=`kubectl get service -n hello -o wide | grep cpx-cic | awk '{ print $3 }'`
 curl http://$CPX_Cluster_IP --header 'Host: www.hello.com'
+kubectl get nodes -o wide
 curl http://$CPX_NODE_IP:NODEPORT --header 'Host: www.hello.com'
+
+kubectl get pods -n foobar -o wide
+CPX_POD_IP=`kubectl get pods -n foobar -o wide | grep cpx-cic | awk '{ print $6 }'`
+curl http://$CPX_POD_IP --header 'Host: www.foobar.com'
+kubectl get service -n foobar -o wide
+CPX_Cluster_IP=`kubectl get service -n foobar -o wide | grep cpx-cic | awk '{ print $3 }'`
+curl http://$CPX_Cluster_IP --header 'Host: www.foobar.com'
+kubectl get nodes -o wide
+curl http://$CPX_NODE_IP:NODEPORT --header 'Host: www.foobar.com'
 
 # Examing LB, try curl several times, you will see CPX is doing the LB
 
+#
+# Tier 2 DONE!
+#
 # Now 2nd tier is ready, let's move to 1st tier VPX/CIC
-# Prepare VPX, same as before. If already configured, skip this
+# We will deploy CICs in both of the namespace to control the tier1 VPX
+# Prepare VPX, same as before.
 add system user cic mypassword
 bind system user cic superuser 0
-add ns ip 172.16.x.x 255.255.255.0 -type SNIP
+add ns ip 172.16.1.54 255.255.255.0 -type SNIP
 
-# create the secret in namespace
-kubectl create secret  generic nslogin --from-literal=username='cic' --from-literal=password='mypassword' -n 2t
+# Back to Master
+# create the secret for tier1 VPX in namespace
+kubectl create secret  generic nslogin-vpx --from-literal=username='cic' --from-literal=password='mypassword' -n hello
+kubectl create secret  generic nslogin-vpx --from-literal=username='cic' --from-literal=password='mypassword' -n foobar
 
 # Deploy the 1st-tier CIC
-kubectl apply -f cic-1tier.yaml -n 2t
+kubectl apply -f cic-1tier-hello.yaml -n hello
+kubectl get pods -n hello
+
+kubectl apply -f cic-1tier-foobar.yaml -n foobar
+kubectl get pods -n foobar
 
 # Create the 1st-tier Ingress object
-kubectl apply -f ingress-1tier.yaml -n 2t
+kubectl apply -f ingress-1tier-hello.yaml -n hello
+kubectl get ingress -n hello
+
+kubectl apply -f ingress-1tier-foobar.yaml -n foobar
+kubectl get ingress -n foobar
 
 # Access the 1st-tier VPX VIP
-curl http://$VPX_VIP --header 'Host: www.hello.com'
+curl http://$VPX_VIP_HELLO --header 'Host: www.hello.com'
+curl http://$VPX_VIP_FOOBAR --header 'Host: www.foobar.com'
+
+# Congratulations! Well Done!
